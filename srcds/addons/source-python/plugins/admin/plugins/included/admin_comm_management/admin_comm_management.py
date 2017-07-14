41ca6102fb1a834436d84f0548b63298af3a36e4
KirillMysnik <kirill@mysnik.com>
2017-07-14 23:55:22 +0300

    Added text chat support to "Admin Communication Management" included plugin

--- srcds/addons/source-python/plugins/admin/plugins/included/admin_comm_management/admin_comm_management.py
+++ srcds/addons/source-python/plugins/admin/plugins/included/admin_comm_management/admin_comm_management.py
@@ -7,6 +7,10 @@ from admin.core.frontends.menus import AdminMenuSection
 
 # Included Plugin
 from .blocks.base import BlockCommAdminCommand
+from .blocks.chat import (
+    block_chat_feature, blocked_chat_user_manager, unblock_any_chat_feature,
+    unblock_my_chat_feature, UnblockAnyChatAdminCommand,
+    UnblockMyChatAdminCommand)
 from .blocks.voice import (
     block_voice_feature, blocked_voice_user_manager, unblock_any_voice_feature,
     unblock_my_voice_feature, UnblockAnyVoiceAdminCommand,
@@ -26,30 +30,30 @@ menu_section_chat = menu_section.add_entry(AdminMenuSection(
 menu_section_voice = menu_section.add_entry(AdminMenuSection(
     menu_section, plugin_strings['section_title voice']))
 
-#menu_section_chat.add_entry(BlockCommAdminCommand(
-#    block_chat_feature,
-#    menu_section_chat,
-#    plugin_strings['popup_title block_chat']))
+menu_section_chat.add_entry(BlockCommAdminCommand(
+    block_chat_feature,
+    menu_section_chat,
+    plugin_strings['popup_title block_chat']))
 
 menu_section_voice.add_entry(BlockCommAdminCommand(
     block_voice_feature,
     menu_section_voice,
     plugin_strings['popup_title block_voice']))
 
-#menu_section_chat.add_entry(UnblockAnyChatAdminCommand(
-#    unblock_any_chat_feature,
-#    menu_section_chat,
-#    plugin_strings['popup_title unblock_chat_any']))
+menu_section_chat.add_entry(UnblockAnyChatAdminCommand(
+    unblock_any_chat_feature,
+    menu_section_chat,
+    plugin_strings['popup_title unblock_chat_any']))
 
 menu_section_voice.add_entry(UnblockAnyVoiceAdminCommand(
     unblock_any_voice_feature,
     menu_section_voice,
     plugin_strings['popup_title unblock_voice_any']))
 
-#menu_section_voice.add_entry(UnblockMyChatAdminCommand(
-#    unblock_my_chat_feature,
-#    menu_section_chat,
-#    plugin_strings['popup_title unblock_chat']))
+menu_section_chat.add_entry(UnblockMyChatAdminCommand(
+    unblock_my_chat_feature,
+    menu_section_chat,
+    plugin_strings['popup_title unblock_chat']))
 
 menu_section_voice.add_entry(UnblockMyVoiceAdminCommand(
     unblock_my_voice_feature,
@@ -59,5 +63,5 @@ menu_section_voice.add_entry(UnblockMyVoiceAdminCommand(
 # =============================================================================
 # >> SYNCHRONOUS DATABASE OPERATIONS
 # =============================================================================
-#blocked_chat_user_manager.refresh()
+blocked_chat_user_manager.refresh()
 blocked_voice_user_manager.refresh()
---
+++ srcds/addons/source-python/plugins/admin/plugins/included/admin_comm_management/blocks/chat.py
@@ -0,0 +1,92 @@
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python
+from commands import CommandReturn
+from commands.say import SayFilter
+
+# Source.Python Admin
+from admin.core.clients import clients
+from admin.core.helpers import log_admin_action
+
+# Included Plugin
+from ..models import BlockedChatUser
+from ..strings import plugin_strings
+from .base import (
+    BlockCommFeature, BlockedCommUserManager, UnblockAnyCommAdminCommand,
+    UnblockCommFeature, UnblockMyCommAdminCommand)
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class _BlockedChatUserManager(BlockedCommUserManager):
+    model = BlockedChatUser
+
+# The singleton object for the _BlockedChatUserManager class.
+blocked_chat_user_manager = _BlockedChatUserManager()
+
+
+class _BlockChatFeature(BlockCommFeature):
+    flag = "admin.admin_comm_management.block_chat"
+    blocked_comm_user_manager = blocked_chat_user_manager
+
+    def execute(self, client, player, duration):
+        super().execute(client, player, duration)
+
+        log_admin_action(plugin_strings['message chat_blocked'].tokenized(
+            admin_name=client.name,
+            player_name=player.name,
+        ))
+
+# The singleton object of the _BlockChatFeature class.
+block_chat_feature = _BlockChatFeature()
+
+
+class _UnblockChatFeature(UnblockCommFeature):
+    feature_abstract = True
+    blocked_comm_user_manager = blocked_chat_user_manager
+
+    def execute(self, client, blocked_comm_user_info):
+        super().execute(client, blocked_comm_user_info)
+
+        log_admin_action(plugin_strings['message chat_unblocked'].tokenized(
+            admin_name=client.name,
+            player_name=blocked_comm_user_info.name,
+        ))
+
+
+class _UnblockAnyChatFeature(_UnblockChatFeature):
+    flag = "admin.admin_comm_management.unblock_any_chat"
+
+# The singleton object of the _UnblockAnyChatFeature class.
+unblock_any_chat_feature = _UnblockAnyChatFeature()
+
+
+class _UnblockMyChatFeature(_UnblockChatFeature):
+    flag = "admin.admin_comm_management.block_chat"
+
+# The singleton object of the _UnblockMyChatFeature class.
+unblock_my_chat_feature = _UnblockMyChatFeature()
+
+
+class UnblockAnyChatAdminCommand(UnblockAnyCommAdminCommand):
+    popup_title = plugin_strings['popup_title unblock_chat_any']
+
+
+class UnblockMyChatAdminCommand(UnblockMyCommAdminCommand):
+    popup_title = plugin_strings['popup_title unblock_chat']
+
+
+# =============================================================================
+# >> COMMAND FILTERS
+# =============================================================================
+@SayFilter
+def say_filter(command, index, team_only):
+    client = clients[index]
+
+    if blocked_chat_user_manager.is_blocked(client.steamid):
+        client.tell(plugin_strings['error chat_block'])
+        return CommandReturn.BLOCK
+
+    return CommandReturn.CONTINUE
